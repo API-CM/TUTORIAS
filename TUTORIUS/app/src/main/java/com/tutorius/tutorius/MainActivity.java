375ed108105b48cae7f8eef8bfd64676d9f92c8f
antjim <email_id>
2017-03-07 12:25:09 +0000

    consultas

--- TUTORIUS/app/src/main/java/com/tutorius/tutorius/MainActivity.java
+++ TUTORIUS/app/src/main/java/com/tutorius/tutorius/MainActivity.java
@@ -1,7 +1,9 @@
 package com.tutorius.tutorius;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void pruebaLanzada(View view){
+        Intent i=new Intent(this,Consulta_Pide_Cita.class);
+        startActivity(i);
     }
 }
