adffe33963b471c34f76192cf4bf3f0a047d55ef
Alvaro Santos <alvaro_santos92@hotmail.com>
2017-03-08 22:50:56 +0100

    Tabs

--- TUTORIUS/app/src/main/java/com/tutorius/tutorius/MainActivity.java
+++ TUTORIUS/app/src/main/java/com/tutorius/tutorius/MainActivity.java
@@ -4,17 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button entrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        entrar = (Button)findViewById(R.id.login);
+
+        entrar.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this,alumnos.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
-    public void pruebaLanzada(View view){
-        Intent i=new Intent(this,Consulta_Pide_Cita.class);
-        startActivity(i);
-    }
 }
---
+++ TUTORIUS/app/src/main/java/com/tutorius/tutorius/page_three.java
@@ -0,0 +1,13 @@
+package com.tutorius.tutorius;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class page_three extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_one);
+    }
+}
