87afcc907cb9702b4790d4e3a8aecdcedb9a63fd
Enrique Pardo Alvarez <enriquezpalvarez@gmail.com>
2017-03-29 13:36:51 +0200

    Prueba de push

---
+++ TUTORIUS/app/src/main/java/com/tutorius/tutorius/Principal_alumno.java
@@ -0,0 +1,13 @@
+package com.tutorius.tutorius;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal_alumno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal_alumno);
+    }
+}
