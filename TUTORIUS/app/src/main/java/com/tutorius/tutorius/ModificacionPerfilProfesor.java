16051b465e71b981ba2268ad7b25e34cdcaf7a3c
Alvaro Santos <alvaro_santos92@hotmail.com>
2017-04-11 09:21:34 +0200

    Merge branch 'master' of https://github.com/API-CM/TUTORIAS into ALVARO
    # Conflicts:
    #	TUTORIUS/app/src/main/java/com/tutorius/tutorius/MainActivity.java

---
+++ TUTORIUS/app/src/main/java/com/tutorius/tutorius/ModificacionPerfilProfesor.java
@@ -0,0 +1,162 @@
+package com.tutorius.tutorius;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ModificacionPerfilProfesor extends AppCompatActivity {
+
+    TextView nombre;
+    TextView departamento;
+    TextView email;
+    TextView horario;
+    TextView despacho;
+    TextView asignaturas;
+
+    private Context mContext;
+    private Activity mActivity;
+    private TextView mCLayout;
+
+    String user;
+    String usuario;
+    // IP de mi Url
+    String IP = "http://ec2-52-39-181-148.us-west-2.compute.amazonaws.com";
+    // Rutas de los Web Services
+    String getProfesor=IP+"/getProfesores.php";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modificacion_perfil_profesor);
+
+        nombre = (TextView) findViewById(R.id.nombre);
+        departamento = (TextView)findViewById(R.id.departamento);
+        email = (TextView)findViewById(R.id.email);
+        horario = (TextView)findViewById(R.id.horario);
+        despacho = (TextView)findViewById(R.id.despacho);
+        asignaturas = (TextView) findViewById(R.id.asignaturas);
+
+        mContext = getApplicationContext();
+        mActivity = ModificacionPerfilProfesor.this;
+        mCLayout = (TextView) findViewById(R.id.errores);
+
+        Bundle b = this.getIntent().getExtras();
+        usuario = b.getString("UVUS");
+
+
+        String cadenallamada = getProfesor + "?uvus_profesor=" + usuario;
+
+
+
+        // Initialize a new RequestQueue instance
+        RequestQueue requestQueue = Volley.newRequestQueue(mContext);
+
+        // Initialize a new JsonObjectRequest instance
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                cadenallamada,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Do something with response
+                        //mTextView.setText(response.toString());
+
+                        // Process the JSON
+                        try{
+                            // Get the JSON array
+                            JSONArray array = response.getJSONArray("profesor");
+                            JSONArray array2 = response.getJSONArray("asignaturas");
+
+                            // Loop through the array elements
+                            for(int i=0;i<array.length();i++){
+                                // Get current json object
+                                JSONObject profesor = array.getJSONObject(i);
+
+                                // Get the current student (json object) data
+                                String firstName = profesor.getString("NOMBRE");
+                                String lastName = profesor.getString("APELLIDO1");
+                                String mail = profesor.getString("EMAIL");
+                                String despa = profesor.getString("DESPACHO");
+                                String depar = profesor.getString("SIGLAS");
+
+                                // Display the formatted json data in text view
+                                nombre.setText(firstName +" " + lastName);
+                                email.setText(mail);
+                                despacho.setText(despa);
+                                departamento.setText(depar);
+
+
+
+                            }
+
+                            for(int i=0;i<array2.length();i++){
+                                // Get current json object
+                                JSONObject asig = array2.getJSONObject(i);
+
+                                String asig2 = asig.getString("NOMBRE");
+
+                                // Display the formatted json data in text view
+                                asignaturas.append(asig2 + "\n");
+
+                            }
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        // Do something when error occurred
+                        Snackbar.make(
+                                mCLayout,
+                                "Error",
+                                Snackbar.LENGTH_LONG
+                        ).show();
+                    }
+                }
+        );
+
+        // Add JsonObjectRequest to the RequestQueue
+        requestQueue.add(jsonObjectRequest);
+
+    }
+
+
+}
