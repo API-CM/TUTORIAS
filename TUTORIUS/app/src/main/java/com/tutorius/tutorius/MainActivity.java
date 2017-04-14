e0aaf0e2ee43e87bf2eb59dee0a2b838db67ce3d
Alvaro Santos <alvaro_santos92@hotmail.com>
2017-04-14 20:02:48 +0200

    Merge branch 'master' of https://github.com/API-CM/TUTORIAS into ALVARO
    # Conflicts:
    #	TUTORIUS/app/src/main/java/com/tutorius/tutorius/MainActivity.java

--- TUTORIUS/app/src/main/java/com/tutorius/tutorius/MainActivity.java
+++ TUTORIUS/app/src/main/java/com/tutorius/tutorius/MainActivity.java
@@ -128,7 +128,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             //Intent intent = new Intent(MainActivity.this,Alumnos.class);
                             //startActivity(intent);
                             devuelve = " ";
-                            Intent intent = new Intent(MainActivity.this,AddAsignaturas.class);
+                            Intent intent = new Intent(MainActivity.this,Alumnos.class);
                             Bundle b = new Bundle();
                             b.putString("UVUS",user1);
                             intent.putExtras(b);
