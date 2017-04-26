2696174c12d2a54acbdb0dfa992751139140f956
Enrique Pardo Alvarez <enriquezpalvarez@gmail.com>
2017-04-26 13:20:25 +0200

    Principal alumno

--- TUTORIUS/app/src/main/java/com/tutorius/tutorius/Datos_cita.java
+++ TUTORIUS/app/src/main/java/com/tutorius/tutorius/Datos_cita.java
@@ -1,6 +1,9 @@
 package com.tutorius.tutorius;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,17 +13,59 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Datos_cita extends AppCompatActivity implements View.OnClickListener{
 
 
+    String cita;
     String usuario;
     Button boton;
+    String fecha;
+    String hora;
+    String id;
+
+    String IP = "http://ec2-52-39-181-148.us-west-2.compute.amazonaws.com";
+
+    private Context mContext;
+    private Activity mActivity;
+    private TextView mCLayout;
+
+    TextView faux;
+    TextView haux;
+    TextView nombre1;
+    TextView departamento1;
+    TextView email1;
+    TextView despacho1;
+    ImageView foto1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datos_cita);
+
+        faux = (TextView)findViewById(R.id.date);
+        haux = (TextView)findViewById(R.id.hour);
+        nombre1 = (TextView) findViewById(R.id.nombre);
+        departamento1 = (TextView)findViewById(R.id.departamento);
+        email1 = (TextView)findViewById(R.id.email);
+        despacho1 = (TextView)findViewById(R.id.despacho);
+        foto1 = (ImageView)findViewById(R.id.imageView);
+
 
         //cabecera
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -28,11 +73,108 @@ public class Datos_cita extends AppCompatActivity implements View.OnClickListene
         //fin cabecera
 
         Bundle b = this.getIntent().getExtras();
-        usuario = b.getString("UVUS");
+        cita = b.getString("CITA");
+        usuario=b.getString("USUARIO");
+
+        String aux[] = cita.split("  -  ");
+        String aux2[] = aux[0].split(" ");
+
+        fecha = aux[1].trim();
+        hora = aux[2].trim();
+        id=aux2[1];
+
+        //----------------------peticion----------------------
+        //-----------------
+        mContext = getApplicationContext();
+        mActivity = Datos_cita.this;
+        mCLayout = (TextView) findViewById(R.id.errores);
+
+        String cadenallamada =  IP + "/getProfesorCitas.php?id_reserva=" + id;
+
+        // Initialize a new RequestQueue instance
+        RequestQueue requestQueue = Volley.newRequestQueue(mContext);
+
+        // Initialize a new JsonObjectRequest instance
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                cadenallamada,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Do something with response
+                        //mTextView.setText(response.toString());
+
+                        // Process the JSON
+                        try{
+                            // Get the JSON array
+                            JSONArray array = response.getJSONArray("profesor");
+                            // JSONArray array2 = response.getJSONArray("despacho");
+
+                            for(int i=0;i<array.length();i++) {
+                                // Get current json object
+                                JSONObject profesor = array.getJSONObject(i);
+
+                                // Get the current student (json object) data
+                                String firstName = profesor.getString("NOMBRE");
+                                String lastName = profesor.getString("APELLIDO1");
+                                String mail = profesor.getString("EMAIL");
+                                String despa = profesor.getString("DESPACHO");
+                                String depar = profesor.getString("SIGLAS");
+                                String urlfoto = profesor.getString("FOTO_PERSONAL");
+
+
+                                // Display the formatted json data in text view
+                                nombre1.setText(firstName + " " + lastName);
+                                despacho1.setText(despa);
+                                departamento1.setText(depar);
+
+                                String site = IP + "/img/" + urlfoto;
+
+                                if (urlfoto != null) {
+                                    RequestQueue colaPeticiones = Volley.newRequestQueue(mContext);
+                                    ImageRequest peticion = new ImageRequest(
+                                            site,
+                                            new Response.Listener<Bitmap>() {
+                                                @Override
+                                                public void onResponse(Bitmap bitmap) {
+                                                    foto1.setImageBitmap(bitmap);
+                                                }
+                                            }, 0, 0, null, null);
+                                    colaPeticiones.add(peticion);
+                                }
+                            }
+
+
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+
+
+                }, new Response.ErrorListener(){
+                    @Override
+                        public void onErrorResponse(VolleyError error){
+                            // Do something when error occurred
+                            Snackbar.make(
+                                    mCLayout,
+                                    "Error",
+                                    Snackbar.LENGTH_LONG
+                            ).show();
+                        }
+                }
+        );
+
+        // Add JsonObjectRequest to the RequestQueue
+        requestQueue.add(jsonObjectRequest);
+
+        faux.setText(fecha);
+        haux.setText(hora);
 
         boton = (Button) findViewById(R.id.boton);
 
         boton.setOnClickListener(this);
+
 
     }
 
@@ -58,7 +200,7 @@ public class Datos_cita extends AppCompatActivity implements View.OnClickListene
 
             case R.id.action_settings_back:
 
-                Intent intent1 = new Intent(Datos_cita.this, MainActivity.class);
+                Intent intent1 = new Intent(Datos_cita.this, Principal_alumno.class);
 
                 b.putString("UVUS",usuario);
                 intent1.putExtras(b);
@@ -67,7 +209,7 @@ public class Datos_cita extends AppCompatActivity implements View.OnClickListene
 
             case R.id.action_settings_home:
 
-                Intent intent2 = new Intent(Datos_cita.this, MainActivity.class);
+                Intent intent2 = new Intent(Datos_cita.this, Principal_alumno.class);
 
                 b.putString("UVUS",usuario);
                 intent2.putExtras(b);
@@ -76,7 +218,7 @@ public class Datos_cita extends AppCompatActivity implements View.OnClickListene
 
             case R.id.action_settings_out:
 
-                Intent intent3 = new Intent(Datos_cita.this, MainActivity.class);
+                Intent intent3 = new Intent(Datos_cita.this, Principal_alumno.class);
                 startActivity(intent3);
                 return true;
 
